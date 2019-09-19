8b7418eee9076a882ee8cfdbdb02c17b3228d47e
Ali <alicertel@gmail.com>
2019-09-19 20:24:23 +0300

    remove unused imports

--- django_ses_plus/utils.py
+++ django_ses_plus/utils.py
@@ -1,11 +1,5 @@
 from time import strftime
 
-from django.template.loader import render_to_string
-from django.utils import translation
-from django.utils.translation import ugettext_lazy as _
-
-from django_ses_plus.settings import DJANGO_SES_PLUS_SETTINGS
-
 
 def sent_email_upload_path(sent_email, filename):
     filename = filename.split(".")[0]
