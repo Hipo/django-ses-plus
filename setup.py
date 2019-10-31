364f36a4c12d33c65fb464f58c8a407fb42733ff
Ali <alicertel@gmail.com>
2019-10-31 19:45:02 +0300

    pump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-ses-plus',
-    version='0.1.0',
+    version='0.1.1',
     packages=['django_ses_plus'],
     include_package_data=True,
     install_requires=['Django >= 1.11', 'django-ses >= 0.8.7', 'celery >= 4'],
