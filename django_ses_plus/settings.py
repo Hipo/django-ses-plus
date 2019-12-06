0415b70eb40e9a0d9878a1c4d9122d1c82d6ccbc
Ömer Faruk Abacı <omerfabaci@gmail.com>
2019-12-06 14:44:47 +0300

    Fix duplicated key, Ignore DEFAULT_FROM_EMAIL if SEND_MAIL is False

--- django_ses_plus/settings.py
+++ django_ses_plus/settings.py
@@ -5,7 +5,7 @@ DJANGO_SES_PLUS_SETTINGS = getattr(settings, "DJANGO_SES_PLUS_SETTINGS", {})
 DJANGO_SES_PLUS_SETTINGS.setdefault("SEND_EMAIL", True)
 
 if DJANGO_SES_PLUS_SETTINGS["SEND_EMAIL"]:
-    DJANGO_SES_PLUS_SETTINGS["CELERY_TASK_RETRY_KWARGS"].setdefault("CELERY_TASK_RETRY_KWARGS", {'max_retries': 15, 'countdown': 60})
+    DJANGO_SES_PLUS_SETTINGS.setdefault("CELERY_TASK_RETRY_KWARGS", {'max_retries': 15, 'countdown': 60})
 
-# Get default from email from django settings.
-DJANGO_SES_PLUS_SETTINGS["DEFAULT_FROM_EMAIL"] = settings.DEFAULT_FROM_EMAIL
+    # Get default from email from django settings.
+    DJANGO_SES_PLUS_SETTINGS["DEFAULT_FROM_EMAIL"] = settings.DEFAULT_FROM_EMAIL
