84ae9203ff8ced3a8012038ac28ee54e10f077b3
Ömer Faruk Abacı <omerfabaci@gmail.com>
2019-12-06 16:49:34 +0300

    Set defaults of all settings

--- django_ses_plus/settings.py
+++ django_ses_plus/settings.py
@@ -3,9 +3,7 @@ from django.conf import settings
 DJANGO_SES_PLUS_SETTINGS = getattr(settings, "DJANGO_SES_PLUS_SETTINGS", {})
 
 DJANGO_SES_PLUS_SETTINGS.setdefault("SEND_EMAIL", True)
+DJANGO_SES_PLUS_SETTINGS.setdefault("CELERY_TASK_RETRY_KWARGS", {'max_retries': 15, 'countdown': 60})
 
-if DJANGO_SES_PLUS_SETTINGS["SEND_EMAIL"]:
-    DJANGO_SES_PLUS_SETTINGS.setdefault("CELERY_TASK_RETRY_KWARGS", {'max_retries': 15, 'countdown': 60})
-
-    # Get default from email from django settings.
-    DJANGO_SES_PLUS_SETTINGS["DEFAULT_FROM_EMAIL"] = settings.DEFAULT_FROM_EMAIL
+# Get default from email from django settings.
+DJANGO_SES_PLUS_SETTINGS.setdefault("DEFAULT_FROM_EMAIL", settings.DEFAULT_FROM_EMAIL)
