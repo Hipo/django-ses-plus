923e16e778895958d709c6c8e2789f4c9f3e1dd9
Ömer Faruk Abacı <omerfabaci@gmail.com>
2019-12-06 12:02:31 +0300

    Ignore DJANGO_SES_PLUS_SETTINGS if SEND_EMAIL is False

--- django_ses_plus/settings.py
+++ django_ses_plus/settings.py
@@ -3,7 +3,9 @@ from django.conf import settings
 DJANGO_SES_PLUS_SETTINGS = getattr(settings, "DJANGO_SES_PLUS_SETTINGS", {})
 
 DJANGO_SES_PLUS_SETTINGS.setdefault("SEND_EMAIL", True)
-DJANGO_SES_PLUS_SETTINGS["CELERY_TASK_RETRY_KWARGS"].setdefault("CELERY_TASK_RETRY_KWARGS", {'max_retries': 15, 'countdown': 60})
+
+if DJANGO_SES_PLUS_SETTINGS["SEND_EMAIL"]:
+    DJANGO_SES_PLUS_SETTINGS["CELERY_TASK_RETRY_KWARGS"].setdefault("CELERY_TASK_RETRY_KWARGS", {'max_retries': 15, 'countdown': 60})
 
 # Get default from email from django settings.
 DJANGO_SES_PLUS_SETTINGS["DEFAULT_FROM_EMAIL"] = settings.DEFAULT_FROM_EMAIL
