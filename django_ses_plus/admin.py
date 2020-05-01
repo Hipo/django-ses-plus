06b43e67302e58da08ab30302c82f391e068d13b
Efe Öge <efeoge@gmail.com>
2020-05-01 14:36:05 +0300

    Resend action is added.

--- django_ses_plus/admin.py
+++ django_ses_plus/admin.py
@@ -1,6 +1,28 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.core.exceptions import ValidationError
 
+from django_ses_plus.settings import DJANGO_SES_PLUS_SETTINGS
+from django_ses_plus.tasks import send_email
 from .models import SentEmail
+
+
+def resend_email(modeladmin, request, queryset):
+    if queryset.count() > 1:
+        raise ValidationError("You should resend email one by one.")
+
+    for sent_email in queryset:
+        html_message = sent_email.html.read().decode("utf-8")
+
+        send_email(
+            subject=sent_email.subject,
+            to_email=sent_email.to_email,
+            from_email=sent_email.from_email,
+            html_message=html_message
+        )
+
+        messages.info(request, f'Email to f{sent_email.to_email} is re-sent.')
+
+resend_email.short_description = "Resend email"
 
 
 class SentEmailAdmin(admin.ModelAdmin):
@@ -16,6 +38,10 @@ class SentEmailAdmin(admin.ModelAdmin):
         actions = super().get_actions(request)
         if 'delete_selected' in actions:
             del actions['delete_selected']
+
+        if DJANGO_SES_PLUS_SETTINGS["CAN_RESEND_FROM_ADMIN_PANEL"]:
+            actions += [resend_email]
+
         return actions
 
     def has_add_permission(self, request):
