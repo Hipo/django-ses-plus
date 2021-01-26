afe4e7fca7f50e8605d834856531ed4dc8f3037e
Ömer Faruk Abacı <omerfabaci@gmail.com>
2021-01-26 14:23:25 +0300

    Bump version to 1.0.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-ses-plus',
-    version='0.2.3',
+    version='1.0.0',
     packages=['django_ses_plus'],
     include_package_data=True,
     install_requires=['Django >= 1.11', 'django-ses >= 1.0.0', 'celery >= 4'],
