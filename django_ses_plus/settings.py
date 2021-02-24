8c8e6c67c33514ce09518902ad6575f74a22c4c3
Furkan Karataş <furkan.karatas02@gmail.com>
2021-02-24 11:41:28 +0300

    Add custom file storage setting: "FILE_STORAGE_BACKEND"

--- django_ses_plus/settings.py
+++ django_ses_plus/settings.py
@@ -4,6 +4,7 @@ DJANGO_SES_PLUS_SETTINGS = getattr(settings, "DJANGO_SES_PLUS_SETTINGS", {})
 
 DJANGO_SES_PLUS_SETTINGS.setdefault("SEND_EMAIL", True)
 DJANGO_SES_PLUS_SETTINGS.setdefault("CELERY_TASK_RETRY_KWARGS", {'max_retries': 15, 'countdown': 60})
+DJANGO_SES_PLUS_SETTINGS.setdefault("FILE_STORAGE_BACKEND", None)
 
 # Get default from email from django settings.
 DJANGO_SES_PLUS_SETTINGS.setdefault("DEFAULT_FROM_EMAIL", settings.DEFAULT_FROM_EMAIL)
--- django_ses_plus/utils.py
+++ django_ses_plus/utils.py
@@ -1,6 +1,15 @@
 from time import strftime
 
+from django.utils.module_loading import import_string
+
+from .settings import DJANGO_SES_PLUS_SETTINGS
+
 
 def sent_email_upload_path(sent_email, filename):
     filename = filename.split(".")[0]
     return strftime("emails/%Y/%m/%d/%Y-%m-%d-%H-%M-%S-{}.html".format(filename))
+
+
+def get_file_storage_backend():
+    if DJANGO_SES_PLUS_SETTINGS["FILE_STORAGE_BACKEND"]:
+        return import_string(DJANGO_SES_PLUS_SETTINGS["FILE_STORAGE_BACKEND"])()
