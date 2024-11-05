f2e95311a23294923b982048c2b1652b147b2434
Ömer Faruk Abacı <omerfabaci@gmail.com>
2024-11-05 19:14:12 +0100

    Email attachment feature (#7)
    * Implement email attachment feature
    
    * Bump version to 1.0.0
    
    * Refactor email attachments feature
    
    * Fix typo
    
    * Email monitoring and tracking (#8)
    
    * add email monitoring and tracking fields
    
    * add bounce type fields
    
    * add migration
    
    * add events extra requirements
    
    * add monitoring fields to SentEmailAdmin
    
    * send_email method returns sent_email
    
    * add ordering to SentEmailAdmin
    
    * if the email is opened, clicked or complained, it is also delivered
    
    * message_id may not be exists in extra_headers for testing
    
    * task required to return a serializable
    
    * Update version
    
    ---------
    
    Co-authored-by: Goksel Coban <goksel326@gmail.com>
    Co-authored-by: Taylan Pince <taylanpince@gmail.com>

--- django_ses_plus/models.py
+++ django_ses_plus/models.py
@@ -1,17 +1,28 @@
+import base64
 import uuid
 
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.dispatch import receiver
 from django.template.loader import render_to_string
 from django.utils import translation
+from django_ses import signals
 from django.utils.translation import gettext_lazy as _
 
 from django_ses_plus.settings import DJANGO_SES_PLUS_SETTINGS
-from .utils import sent_email_upload_path
+from .utils import sent_email_upload_path, sent_email_attachment_upload_path
 
 
 class SentEmail(models.Model):
+    class Status(models.TextChoices):
+        UNKNOWN = 'unknown', 'Unknown'
+        SENT = 'sent', 'Sent'
+        DELIVERED = 'delivered', 'Delivered'
+        BOUNCED = 'bounced', 'Bounced'
+        # Don't support rejects and rendering failures for now.
+        # REJECTED = 'rejected', 'Rejected'
+        # RENDERING_FAILURE = 'rendering-failure', 'Rendering Failure'
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
@@ -20,6 +31,15 @@ class SentEmail(models.Model):
     from_email = models.EmailField()
     subject = models.TextField()
     html = models.FileField(upload_to=sent_email_upload_path)
+
+    # tracking & monitoring
+    message_id = models.CharField(max_length=100, blank=True)
+    status = models.CharField(choices=Status.choices, default=Status.UNKNOWN, max_length=50)
+    bounce_type = models.CharField(max_length=50, blank=True)
+    bounce_sub_type = models.CharField(max_length=50, blank=True)
+    is_opened = models.BooleanField(default=False)
+    is_clicked = models.BooleanField(default=False)
+    is_complained = models.BooleanField(default=False)
 
     creation_datetime = models.DateTimeField(auto_now_add=True)
     update_datetime = models.DateTimeField(auto_now=True)
@@ -31,13 +51,107 @@ class SentEmail(models.Model):
     def __str__(self):
         return f"{self.subject} to {self.to_email}"
 
+    @staticmethod
+    @receiver(signals.send_received)
+    def send_handler(sender, mail_obj, send_obj, **kwargs):
+        message_id = mail_obj['messageId']
+        SentEmail.objects.filter(
+            message_id=message_id,
+            status=SentEmail.Status.UNKNOWN,
+        ).update(
+            status=SentEmail.Status.SENT
+        )
+
+    @staticmethod
+    @receiver(signals.delivery_received)
+    def delivery_handler(sender, mail_obj, delivery_obj, **kwargs):
+        message_id = mail_obj['messageId']
+        recipients = delivery_obj['recipients']
+        SentEmail.objects.filter(
+            message_id=message_id,
+            to_email__in=recipients,
+        ).update(
+            status=SentEmail.Status.DELIVERED
+        )
+
+    @staticmethod
+    @receiver(signals.bounce_received)
+    def bounce_handler(sender, mail_obj, bounce_obj, **kwargs):
+        message_id = mail_obj['messageId']
+        recipients = [r['emailAddress'] for r in bounce_obj['bouncedRecipients']]
+        SentEmail.objects.filter(
+            message_id=message_id,
+            to_email__in=recipients,
+        ).update(
+            status=SentEmail.Status.BOUNCED,
+            bounce_type=bounce_obj['bounceType'],
+            bounce_sub_type=bounce_obj['bounceSubType'],
+        )
+
+    @staticmethod
+    @receiver(signals.open_received)
+    def open_handler(sender, mail_obj, open_obj, **kwargs):
+        # open_obj does not provide recipient emails.
+        message_id = mail_obj['messageId']
+        SentEmail.objects.filter(
+            message_id=message_id,
+        ).update(
+            status=SentEmail.Status.DELIVERED,
+            is_opened=True
+        )
+
+    @staticmethod
+    @receiver(signals.click_received)
+    def click_handler(sender, mail_obj, click_obj, **kwargs):
+        # click_obj does not provide recipient emails.
+        message_id = mail_obj['messageId']
+        SentEmail.objects.filter(
+            message_id=message_id,
+        ).update(
+            status=SentEmail.Status.DELIVERED,
+            is_clicked=True
+        )
+
+    @staticmethod
+    @receiver(signals.complaint_received)
+    def compliant_handler(sender, mail_obj, complaint_obj, **kwargs):
+        message_id = mail_obj['messageId']
+        recipients = complaint_obj['complainedRecipients']
+        SentEmail.objects.filter(
+            message_id=message_id,
+            to_email__in=recipients,
+        ).update(
+            status=SentEmail.Status.DELIVERED,
+            is_complained=True
+        )
+
+
+class SentEmailAttachment(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    sent_email = models.ForeignKey(to="django_ses_plus.SentEmail", related_name="attachments", on_delete=models.CASCADE)
+    filename = models.CharField(max_length=255)
+    content = models.FileField(upload_to=sent_email_attachment_upload_path)
+    mimetype = models.CharField(help_text="e.g. text/html, application/pdf, image/png...", max_length=255)
+
+    creation_datetime = models.DateTimeField(auto_now_add=True)
+    update_datetime = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = _("Sent Email Attachment")
+        verbose_name_plural = _("Sent Email Attachments")
+
+    def __str__(self):
+        return f"{self.filename} ({self.mimetype})"
+
 
 class SendEmailMixin(object):
 
     def get_to_email(self):
         return self.email
 
-    def send_email(self, subject, template_path, context, from_email=None, language=None):
+    def send_email(self, subject, template_path, context, from_email=None, language=None, attachments=None):
         from .tasks import send_email
         if not DJANGO_SES_PLUS_SETTINGS["SEND_EMAIL"]:
             return _("Email cannot be sent due to SEND_EMAIL flag in project settings.")
@@ -50,11 +164,25 @@ class SendEmailMixin(object):
         if language:
             translation.activate(language)
 
+        if attachments is not None:
+            assert isinstance(attachments, list), "Attachments should be a `list` of `dict` objects."
+
+            for attachment in attachments:
+                assert all([key in attachment for key in ["filename", "content", "mimetype"]]), "Attachments should contain `filename`, `content` and `mimetype`."
+
+                if isinstance(attachment["content"], bytes):
+                    # Since celery only accepts JSON serializable types and `bytes` is not JSON serializable,
+                    # Base64 encoding is used to be able to pass attachment content to the celery task,
+                    attachment["content"] = base64.b64encode(attachment["content"]).decode("utf-8")
+                else:
+                    assert False, f"Attachment contents should be `bytes`, not {type(attachment['content'])}."
+
         html_message = render_to_string(template_path, context)
         send_email.delay(
             subject=subject,
             to_email=self.get_to_email(),
             html_message=html_message,
+            attachments=attachments,
             from_email=from_email,
             recipient_id=recipient_id
         )
--- django_ses_plus/utils.py
+++ django_ses_plus/utils.py
@@ -1,6 +1,33 @@
+import base64
+from os import path
 from time import strftime
+from uuid import uuid4
+
+from django.core.mail import get_connection
+from django.core.mail.message import EmailMultiAlternatives
 
 
 def sent_email_upload_path(sent_email, filename):
     filename = filename.split(".")[0]
     return strftime("emails/%Y/%m/%d/%Y-%m-%d-%H-%M-%S-{}.html".format(filename))
+
+
+def sent_email_attachment_upload_path(sent_email_attachment, filename):
+    extension = path.splitext(filename)[1]
+    return strftime(f"email-attachments/%Y/%m/%d/{uuid4().hex}{extension}")
+
+
+def send_mail(subject, message, from_email, recipient_list, fail_silently=False, html_message=None, attachments=None):
+    connection = get_connection(fail_silently=fail_silently)
+
+    mail = EmailMultiAlternatives(subject, message, from_email, recipient_list, connection=connection)
+
+    if html_message:
+        mail.attach_alternative(html_message, 'text/html')
+
+    if attachments:
+        for attachment in attachments:
+            mail.attach(attachment["filename"], base64.b64decode(attachment["content"]), attachment["mimetype"])
+
+    num_sent = mail.send()
+    return num_sent, mail
